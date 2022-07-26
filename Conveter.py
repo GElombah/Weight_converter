d293b2e1c7599a5670d1d120455880264a046500
Godswill Elombah <godswillbob@gmail.com>
2022-07-26 15:14:00 +0100

    Update Conveter.py

--- Conveter.py
+++ Conveter.py
@@ -26,12 +26,20 @@ e1_value=StringVar()
 e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0,column=1)
 
+e1=Label(window,text="Ib")
+e1.grid(row=1,column=0)
 t1=Text(window, height=1, width=20)
 t1.grid(row=1,column=0)
 
+
+e1=Label(window,text="g")
+e1.grid(row=2,column=0)
 t2=Text(window, height=1, width=20)
 t2.grid(row=1,column=1)
 
+
+e1=Label(window,text="oz")
+e1.grid(row=3,column=0)
 t3=Text(window, height=1, width=20)
 t3.grid(row=1,column=2)
 window.mainloop()
